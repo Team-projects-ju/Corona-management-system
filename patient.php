e70be498d21eedaba7eab48ea88ef7cf9717b1dd
mohitz007 <mohitsmart2004@gmail.com>
2020-10-21 16:15:17 +0530

    now showing no of beds

--- patient.php
+++ patient.php
@@ -42,14 +42,10 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
     left: 70px;
 }">
     <button onclick="getLocation()">Share your location</button>
-        <div id="output"></div>
-        <div class="card a"><i class="fa fa-tachometer"></i><h5>Total no of empty beds available - 54</h5><span id="active"></span></div>
-      <div class="card ca"><i class="fa fa-th-list"></i><h5>Total no of empty beds available - 54</h5><span id="cases"></span></div>
-      <div class="card cr"><i class="fa fa-times-circle"></i><h5>Total no of empty beds available - 82</h5><span id="critical"></span></div>
-      <div class="card d"><i class="fa fa-times"></i><h5>Total no of empty beds available -11</h5><span id="death"></span></div>
-      <div class="card r"><i class="fa fa-check-square-o"></i><h5>Total no of empty beds available - 12</h5><span id="recovered"></span></div>
-      <div class="card t"><i class="fa fa-eye"></i><h5>Total no of empty beds available - 18</h5><span id="tests"></span></div>
-    </div>
+        <div id="output">
+          
+        </div>
+        
 </section>
     <script>
         var x = document.getElementById('output');
@@ -68,8 +64,8 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
         function showPosition(position) {
         latitude = position.coords.latitude; 
         longitude = position.coords.longitude;
-        x.innerHTML = "Now sending your coordinates";
-        x.innerHTML = longitude + " " + " " + latitude;
+        // x.innerHTML = "Now sending your coordinates";
+        // x.innerHTML = longitude + " " + " " + latitude;
         
         var url = 'https://cors-anywhere.herokuapp.com/https://services.gisgraphy.com/geoloc/search?lat='+latitude+'&lng='+longitude+'&radius=7000&placetype=hospital&format=json';
         fetch(url)
@@ -80,14 +76,21 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
   )
   .then((data) => {
     console.log(data);
-    var json = data;
-    document.getElementById("output").innerHTML = data.result[0].name;    
-    document.getElementById("active").innerHTML = data.result[1].name;
-    document.getElementById("cases").innerHTML = data.result[2].name;
-    document.getElementById("critical").innerHTML = data.result[3].name;
-    document.getElementById("death").innerHTML = data.result[4].name;
-    document.getElementById("recovered").innerHTML = data.result[5].name;
-    document.getElementById("tests").innerHTML =" ";
+    var size = data.numFound;
+    var str = "<table><thead><td>Hospital Name</td><td>Beds available</td></thead>";
+    // document.getElementById("output").innerHTML = data.result[0].name;    
+    // document.getElementById("active").innerHTML = data.result[1].name;
+    // document.getElementById("cases").innerHTML = data.result[2].name;
+    // document.getElementById("critical").innerHTML = data.result[3].name;
+    // document.getElementById("death").innerHTML = data.result[4].name;
+    // document.getElementById("recovered").innerHTML = data.result[5].name;
+    // document.getElementById("tests").innerHTML =" ";
+    for(var i = 0; i < size ;i++)
+    {
+      str+="<tr><td>"+data.result[i].name+"</td><td>"+Math.floor((Math.random() * 100) + 1)+"</td></tr>";
+    }
+    str+="</table>";
+    document.getElementById("output").innerHTML = str;
     
   });
         }
